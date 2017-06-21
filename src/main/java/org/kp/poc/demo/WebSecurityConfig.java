794f950b4101195bcbf439db92649be05760a65f
Robert Tu <robert.f.tu@gmail.com>
2017-06-21 06:08:47 -0700

    Add JUnit Test and refactor code on WebSecurityConfig

--- src/main/java/org/kp/poc/demo/WebSecurityConfig.java
+++ src/main/java/org/kp/poc/demo/WebSecurityConfig.java
@@ -13,6 +13,9 @@ import org.springframework.security.config.annotation.authentication.builders.Au
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.configurers.ExpressionUrlAuthorizationConfigurer;
+import org.springframework.security.config.annotation.web.configurers.FormLoginConfigurer;
+//import org.springframework.security.config.annotation.web.configurers.LogoutConfigurer;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
@@ -34,11 +37,45 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		logger.info("Initialize Web Security");
-		http.authorizeRequests()
+		
+		// Java translation for: Spring Security applicationContext-security.xml
+		/*
+		 <security:http auto-config="true" access-denied-page="/auth/denied">
+    	<security:intercept-url pattern="/admin/*" access="ROLE_ADMIN"/>
+    	<security:intercept-url pattern="/import/*" access="ROLE_ADMIN"/>
+    	<security:intercept-url pattern="/user/*" access="ROLE_USER"/>
+    	<security:intercept-url pattern="/auth/login" access="IS_AUTHENTICATED_ANONYMOUSLY"/>
+    	<security:intercept-url pattern="/auth/register" access="IS_AUTHENTICATED_ANONYMOUSLY"/>
+    	<security:intercept-url pattern="/**" access="IS_AUTHENTICATED_ANONYMOUSLY"/>
+    	<security:form-login login-page="/auth/login" 
+	   	authentication-failure-url="/auth/login?login_error=true"
+    	default-target-url="/user"/>
+    	<security:logout logout-url="/auth/logout" logout-success-url="/" invalidate-session="true"/>
+		</security:http>
+		 */
+		
+		// Get URL Registry
+		ExpressionUrlAuthorizationConfigurer<HttpSecurity>.ExpressionInterceptUrlRegistry urlRegistry = http.authorizeRequests();
+		
+		// Create configuration for Authorized URLS. Allows non-authenticated access to pre-defined URL in Constants.AUTHORIZED_URLS
+		ExpressionUrlAuthorizationConfigurer<HttpSecurity>.AuthorizedUrl authorizedUrls = urlRegistry.antMatchers(Constants.AUTHORIZED_URLS).permitAll().anyRequest();
+		
+		// Forward to "/" for authenticated User. Non authenticated to be redirected to Login Page "/login
+		FormLoginConfigurer<HttpSecurity> loginConfigurer = authorizedUrls.authenticated().
+				and().
+				formLogin().loginPage("/login").successForwardUrl("/");
+	
+		// Login error redirects to /login?error and log the user out		
+		// After logout, then delete cookies. Logout successful then redirects to /login
+		loginConfigurer.failureUrl("/login?error").usernameParameter("username").permitAll().
+				and().
+				logout().logoutUrl("/logout").deleteCookies("remember-me").logoutSuccessUrl("/login").permitAll();
+		
+		/*http.authorizeRequests()
 				.antMatchers(Constants.AUTHORIZED_URLS)
 				.permitAll().anyRequest().authenticated().and().formLogin().loginPage("/login").successForwardUrl("/")
 				.failureUrl("/login?error").usernameParameter("username").permitAll().and().logout()
-				.logoutUrl("/logout").deleteCookies("remember-me").logoutSuccessUrl("/login").permitAll();
+				.logoutUrl("/logout").deleteCookies("remember-me").logoutSuccessUrl("/login").permitAll();*/
 		http.csrf().disable();
 	}
 	
--- src/test/java/org/kp/poc/demo/KaiserPocApplicationTests.java
+++ src/test/java/org/kp/poc/demo/KaiserPocApplicationTests.java
@@ -1,16 +1,115 @@
 package org.kp.poc.demo;
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Optional;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.kp.poc.demo.model.Role;
+import org.kp.poc.demo.model.User;
+import org.kp.poc.demo.repository.RoleRepository;
+import org.kp.poc.demo.service.UserServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.crypto.bcrypt.BCrypt;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class KaiserPocApplicationTests {
-
+	private String testUserName = "user3";
+	private Role userRole;
+	private Role adminRole;
+	private User testUser = null;
+	
+	@Autowired
+	private UserServiceImpl userService;
+	
+	@Autowired
+	private RoleRepository roleRepository;
+	
+	@BeforeClass
+	public static void setUp() {
+		System.out.println("----- Setup Test here -----");
+	}
+	
+	@Before
+	public void preTest() {
+		System.out.println("*********************Initialize User Test Objects");
+		Optional<User> optUser = userService.getUserByUsername(testUserName);
+		if(optUser.isPresent()) {
+			// If this Record was not cleaned up from the last test, then delete it
+			System.out.println("*********************Found Test User. Delete");
+			userService.deleteUser(optUser.get().getId());
+		}
+		// Load/Init Roles
+		this.userRole = getRole("USER");
+		this.adminRole = getRole("ADMIN");
+	}
+	
 	@Test
 	public void contextLoads() {
+		assertNotNull("User Role is not null", userRole);
+		System.out.println("*********************Found User Role: " + userRole.getRoleName());
+		assertNotNull("User Role is not null", adminRole);
+		System.out.println("*********************Found Admin Role: " + adminRole.getRoleName());
+				
+		System.out.println("*********************Creating Test User");
+
+		testUser = createUser(testUserName);
+		
+		Collection<Role> roles = new ArrayList<Role>();
+        roles.add(userRole);
+        roles.add(adminRole);
+        testUser.setRole(roles);
+        testUser.setEnabled(true);
+        String plainTextPassword = testUser.getPassword();  // For testing after Save
+        
+        User savedTestUser = userService.create(testUser);
+        System.out.println("*********************Test User ID: " + savedTestUser.getId());
+		System.out.println("*********************Test User Name: " + savedTestUser.getUsername());
+		System.out.println("*********************Test User Password: " + savedTestUser.getPassword());
+		
+		assertNotNull("User is not null", savedTestUser);
+		
+		// Check Encrypted Password
+		String encryptedPassword = savedTestUser.getPassword();
+		assertTrue(BCrypt.checkpw(plainTextPassword, encryptedPassword));
+	}
+	
+	protected User createUser(String username) {
+        User user = new User();
+        //all the user fields
+        user.setUsername(username);
+        user.setPassword("testing");
+        user.setFirst_name("TestFirst");
+        user.setLast_name("TestLast");
+        user.setEmail("test.user@mycompany.com");
+        return user;
+    }
+	
+	private Role getRole(String name) {
+		return roleRepository.findRoleByRoleName(name);
+	}
+	
+	@After
+	public void cleanUp() {
+		System.out.println("*********************Cleanup/Delete Test User");
+		if(testUser != null) {
+			userService.deleteUser(testUser.getId());
+		}
+	}
+	
+	@AfterClass
+	public static void afterTest() {
+		System.out.println("----- End Kaiser User Creation Tests -----");
 	}
 
 }
